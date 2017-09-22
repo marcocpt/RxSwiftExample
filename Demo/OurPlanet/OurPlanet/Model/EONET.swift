a0630dc1996903c20c2f68cfd72c8d330bdbc9fd
wgd <marcogithun@163.com>
2017-09-22 14:36:55 +0800

    请求无响应（500）
    https://eonet.sci.gsfc.nasa.gov/ 网站请求无响应（500）

--- Demo/OurPlanet/OurPlanet/Model/EONET.swift
+++ Demo/OurPlanet/OurPlanet/Model/EONET.swift
@@ -37,19 +37,7 @@ class EONET {
     return formatter
   }()
 
-	static var categories: Observable<[EOCategory]> = {
-		return EONET.request(endpoint: categoriesEndpoint)
-			.map { data in
-				let categories = data["categories"] as? [[String: Any]] ?? []
-				return categories
-					.flatMap(EOCategory.init)
-					.sorted { $0.name < $1.name }
-			}
-			.shareReplay(1)
-	}()
-
-  static func filteredEvents(events: [EOEvent],
-                              forCategory category: EOCategory) -> [EOEvent] {
+  static func filteredEvents(events: [EOEvent], forCategory category: EOCategory) -> [EOEvent] {
     return events.filter { event in
       return event.categories.contains(category.id) &&
              !category.events.contains {
@@ -58,16 +46,13 @@ class EONET {
     }
     .sorted(by: EOEvent.compareDates)
   }
-  
-  static func request(endpoint: String, query: [String: Any] = [:]) ->
-		Observable<[String: Any]> {
+
+  static func request(endpoint: String, query: [String: Any] = [:]) -> Observable<[String: Any]> {
     do {
-      guard let url = URL(string: API)?.appendingPathComponent(endpoint)
-        , var components = URLComponents(url: url,
-                                         resolvingAgainstBaseURL: true) else {
+      guard let url = URL(string: API)?.appendingPathComponent(endpoint),
+        var components = URLComponents(url: url, resolvingAgainstBaseURL: true) else {
           throw EOError.invalidURL(endpoint)
       }
-      
       components.queryItems = try query.flatMap { (key, value) in
         guard let v = value as? CustomStringConvertible else {
           throw EOError.invalidParameter(key, value)
@@ -77,45 +62,54 @@ class EONET {
       guard let finalURL = components.url else {
         throw EOError.invalidURL(endpoint)
       }
-      
-			let request = URLRequest(url: finalURL)
+      let request = URLRequest(url: finalURL)
 
-			return URLSession.shared.rx.response(request: request)
-				.map { _, data -> [String: Any] in
-					guard let jsonObject = try? JSONSerialization.jsonObject(with: data,
-					                                                         options: []),
-						let result = jsonObject as? [String: Any] else {
-							throw EOError.invalidJSON(finalURL.absoluteString)
-					}
-					return result
-			}
+      return URLSession.shared.rx.response(request: request)
+        .map { _, data -> [String: Any] in
+          guard let jsonObject = try? JSONSerialization.jsonObject(with: data, options: []),
+            let result = jsonObject as? [String: Any] else {
+              throw EOError.invalidJSON(finalURL.absoluteString)
+          }
+          return result
+      }
     } catch {
       return Observable.empty()
     }
   }
 
-	fileprivate static func events(forLast days: Int, closed: Bool) ->
-		Observable<[EOEvent]> {
-			return request(endpoint: eventsEndpoint, query: [
-				"days": NSNumber(value: days),
-				"status": (closed ? "closed" : "open")
-				])
-				.map { json in
-					guard let raw = json["events"] as? [[String: Any]] else {
-						throw EOError.invalidJSON(eventsEndpoint)
-					}
-					return raw.flatMap(EOEvent.init)
-			}
-	}
+  static var categories: Observable<[EOCategory]> = {
+    return EONET.request(endpoint: categoriesEndpoint)
+      .map { data in
+        let categories = data["categories"] as? [[String: Any]] ?? []
+        return categories
+          .flatMap(EOCategory.init)
+          .sorted { $0.name < $1.name }
+      }
+      .shareReplay(1)
+  }()
 
-	static func events(forLast days: Int = 360) -> Observable<[EOEvent]> {
-		let openEvents = events(forLast: days, closed: false)
-		let closedEvents = events(forLast: days, closed: true)
-		return Observable.of(openEvents, closedEvents)
-			.merge()
-			.reduce([]) { running, new in
-				running + new
-		}
-	}
+  fileprivate static func events(forLast days: Int, closed: Bool, endpoint: String) -> Observable<[EOEvent]> {
+    return request(endpoint: endpoint, query: [
+      "days": NSNumber(value: days),
+      "status": (closed ? "closed" : "open")
+      ])
+    .map { json in
+      guard let raw = json["events"] as? [[String: Any]] else {
+        throw EOError.invalidJSON(endpoint)
+      }
+      return raw.flatMap(EOEvent.init)
+    }
+  }
+
+  static func events(forLast days: Int = 360, category: EOCategory) -> Observable<[EOEvent]> {
+    let openEvents = events(forLast: days, closed: false, endpoint: category.endpoint)
+    let closedEvents = events(forLast: days, closed: true, endpoint: category.endpoint)
+    
+    return Observable.of(openEvents, closedEvents)
+      .merge()
+      .reduce([]) { running, new in
+        running + new
+    }
+  }
 
 }
